b74d4910a72b7dcf3f2d8352f706a7f27069dbcb
c1m50c <58411864+c1m50c@users.noreply.github.com>
2022-07-27 17:43:19 -0500

    conversion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
+
 fn main() {
+    let mut args = env::args().skip(1);
+
+    let input_img_path = args.next()
+        .expect("Expected an Input Image File Path argument.");
+    let output_img_path = args.next()
+        .expect("Expected an Output Image File Path argument.");
+    let _filter_names = args.collect::<Vec<_>>();
+
+    let img = image::open(input_img_path)
+        .unwrap_or_else(|err| {
+            panic!("Failed to open Image from `input_img_path` | Error {:?}", err)
+        });
     
+    img.save(output_img_path)
+        .unwrap_or_else(|err| {
+            panic!("Failed to save Image from `output_img_path` | Error {:?}", err)
+        });
 }
